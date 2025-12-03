b573b0a6295a4fd319cf8a540c5561a19b8ae774
AMR1234567891011 <111772107+AMR1234567891011@users.noreply.github.com>
2025-12-03 16:06:19 -0600

    emcc compile script im going insane

--- demo/compile.sh
+++ demo/compile.sh
@@ -3,6 +3,6 @@ emcc -O3 -sWASM=1 \
   -s'EXPORTED_FUNCTIONS=[_x3dh_woR,_x3dh_woS,_main_tests,_wasm_init_sessions,_wasm_new_session,_wasm_send_message,_wasm_receive_message,_malloc,_free]' \
   -s'INITIAL_MEMORY=67108864' \
   -s'ALLOW_MEMORY_GROWTH=1' \
-  -s'EXTRA_EXPORTED_RUNTIME_METHODS=["cwrap"]' \
+  -s'EXPORTED_RUNTIME_METHODS=["cwrap"]' \
   -o crypto_wasm.js \
   ../main_test.c ../sha256.c ../X25519.c ../X3DH.c ../session_manager.c ../session_test.c ../wasm_interface.c
