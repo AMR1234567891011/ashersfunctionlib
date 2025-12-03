02581b82aedbeb22bffa1618b6227cadf3f9e2d1
AMR1234567891011 <111772107+AMR1234567891011@users.noreply.github.com>
2025-12-03 16:46:52 -0600

    please work

--- demo/compile.sh
+++ demo/compile.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-emcc -O3 -sWASM=1 -v \
-  -s'EXPORTED_FUNCTIONS=[_x3dh_woR,_x3dh_woS,_main_tests,_wasm_init_sessions,_wasm_new_session,_wasm_send_message,_wasm_receive_message,_malloc,_free]' \
-  -s'INITIAL_MEMORY=67108864' \
-  -s'ALLOW_MEMORY_GROWTH=1' \
-  -s'EXPORTED_RUNTIME_METHODS=["cwrap", "HEAPU8", "buffer"]' \
-  -g4 -s ASSERTIONS=2 \
-  -o crypto_wasm.js \
+
+emcc -O3 \
+  -sWASM=1 \
+  -sEXPORTED_FUNCTIONS='[_x3dh_woR,_x3dh_woS,_main_tests,_wasm_init_sessions,_wasm_new_session,_wasm_send_message,_wasm_receive_message,_malloc,_free]' \
+  -sINITIAL_MEMORY=67108864 \
+  -sALLOW_MEMORY_GROWTH=1 \
+  -sEXPORTED_RUNTIME_METHODS='["cwrap", "HEAPU8", "HEAP8", "UTF8ToString", "stringToUTF8", "lengthBytesUTF8"]' \
+  -g \
+  -sASSERTIONS=2 \
+  -sERROR_ON_UNDEFINED_SYMBOLS=0 \
+  -o static/crypto_wasm.js \
   ../main_test.c ../sha256.c ../X25519.c ../X3DH.c ../session_manager.c ../session_test.c ../wasm_interface.c
 
-mkdir static
-mv crypto_wasm.js static
-mv crypto_wasm.wasm static
+mv crypto_wasm.wasm ./static
+mv crypto_wasm.js ./static
