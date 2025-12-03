c651dad326d9ac931dbc2bd11c6a883c31b66580
AMR1234567891011 <111772107+AMR1234567891011@users.noreply.github.com>
2025-12-03 16:52:57 -0600

    i hate emcc

--- demo/compile.sh
+++ demo/compile.sh
@@ -2,7 +2,7 @@
 
 emcc -O3 \
   -sWASM=1 \
-  -sEXPORTED_FUNCTIONS='[_x3dh_woR,_x3dh_woS,_main_tests, scalar_mult, _wasm_init_sessions,_wasm_new_session,_wasm_send_message,_wasm_receive_message,_malloc,_free]' \
+  -sEXPORTED_FUNCTIONS='[_x3dh_woR,_x3dh_woS,_main_tests, _scalar_mult, _wasm_init_sessions,_wasm_new_session,_wasm_send_message,_wasm_receive_message,_malloc,_free]' \
   -sINITIAL_MEMORY=67108864 \
   -sALLOW_MEMORY_GROWTH=1 \
   -sEXPORTED_RUNTIME_METHODS='["cwrap", "HEAPU8", "HEAP8", "UTF8ToString", "stringToUTF8", "lengthBytesUTF8"]' \
