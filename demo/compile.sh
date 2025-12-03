aaabba63884a371c412ac15fdea42367efe11a06
AMR1234567891011 <111772107+AMR1234567891011@users.noreply.github.com>
2025-12-03 15:54:44 -0600

    emscripten compilation fix

--- demo/compile.sh
+++ demo/compile.sh
@@ -3,10 +3,13 @@
 echo "=== Compiling Crypto Libraries ==="
 
 # Compile WASM for JS
-echo "Building WebAssembly..."
+echo "Building WebAssembly with Memory Exports..."
+
 emcc -O3 -s WASM=1 \
   -s EXPORTED_FUNCTIONS="[_x3dh_woR,_x3dh_woS,_main_tests,_wasm_init_sessions,_wasm_new_session,_wasm_send_message,_wasm_receive_message,_malloc,_free]" \
-  -s EXPORTED_RUNTIME_METHODS="[ccall,cwrap]" \
+  -s **INITIAL_MEMORY**=67108864 \
+  -s **ALLOW_MEMORY_GROWTH**=1 \
+  -s **EXTRA_EXPORTED_RUNTIME_METHODS**="['cwrap']" \
   -o crypto_wasm.js \
   ../main_test.c ../sha256.c ../X25519.c ../X3DH.c ../session_manager.c ../session_test.c ../wasm_interface.c
 
