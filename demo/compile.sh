9e136056acca8b374024aca3296b0670e5bf21a7
AMR1234567891011 <111772107+AMR1234567891011@users.noreply.github.com>
2025-12-03 16:25:37 -0600

    hopeless

--- demo/compile.sh
+++ demo/compile.sh
@@ -4,5 +4,10 @@ emcc -O3 -sWASM=1 \
   -s'INITIAL_MEMORY=67108864' \
   -s'ALLOW_MEMORY_GROWTH=1' \
   -s'EXPORTED_RUNTIME_METHODS=["cwrap"]' \
+  -g4 -s ASSERTIONS=2 \
   -o crypto_wasm.js \
   ../main_test.c ../sha256.c ../X25519.c ../X3DH.c ../session_manager.c ../session_test.c ../wasm_interface.c
+
+mkdir static
+mv crypto_wasm.js static
+mv crypto_wasm.wasm static
